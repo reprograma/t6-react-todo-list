58483f6686bd55e2fe02b5f3504168983af898fa
{Reprograma} <reprograma@MacBook-Pro-Reprograma-2.local>
2018-11-21 14:14:28 -0200

    wip

--- my-app/src/components/postit/postit.js
+++ my-app/src/components/postit/postit.js
@@ -7,6 +7,9 @@ class Postit extends React.Component {
     constructor(props){
         super(props)
         this.state = {
+            id: 0,
+            title: '',
+            text: '',
             editing : false
         }
     }
@@ -19,10 +22,30 @@ class Postit extends React.Component {
     handlePostitRemove = () =>{
         console.log('handlePostitRemove')
     }
+    handlePostitSubmit = (e) => {
+        e.preventDefault()
+        const postit = {
+            title :  this.state.title,
+            text :  this.state.text
+        }
+    }
+    setTitle = (e) => {
+        const inputTitle = e.target.value
+        console.log('evento ',e)
+        this.setState({
+            title : inputTitle
+        })
+    }
+    setText = (event) => {
+        const inputText = event.target.value
+        this.setState({
+            text :  inputText
+        })
+    }
     render() {
         return (
             <div onClick={this.handlePostitClick} className='postit'>
-                <Form>
+                <Form onSubmit={this.handlePostitSubmit}>
                     {this.state.editing  && (
                                 <button 
                                     onClick={this.handlePostitRemove} 
@@ -37,11 +60,16 @@ class Postit extends React.Component {
                         type='text' 
                         className='postit__title' 
                         placeholder='Título' 
+                        name='title'
+                        value={this.state.title}
+                        onChange={this.setTitle}
                    /> 
                    <textarea 
                         className='postit__text'
                         placeholder='Digite o texto...'
                         name='text'
+                        value={this.state.text}
+                        onChange={this.setText}
                    />    
                    {this.state.editing && 
                             (<button className='postit__button-completed'>
